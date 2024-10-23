10d64201954bcef6251e62c46f022e670766b332
zuby <65686164+KeyProcedure@users.noreply.github.com>
2024-10-22 18:15:23 -0700

    Create development environment

---
+++ src/server/index.ts
@@ -0,0 +1,33 @@
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import express from "express";
+import httpErrors from "http-errors";
+import morgan from "morgan";
+import * as path from "path";
+import {timeMiddleware} from "./middleware/time";
+
+import rootRoutes from "./routes/root";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(process.cwd(), "src", "public")));
+app.use(cookieParser());
+app.set("views", path.join(process.cwd(), "src", "server", "views"));
+app.set("view engine", "ejs");
+
+app.use("/", rootRoutes);
+
+app.use((_request, _response, next) => {
+    next(httpErrors(404, "Page Not Found"));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
