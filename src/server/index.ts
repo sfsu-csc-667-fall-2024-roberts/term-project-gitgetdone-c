cf4f7ddc04dc034bf78a7a396f6cd9f96b20f4cd
zuby <65686164+KeyProcedure@users.noreply.github.com>
2024-11-15 18:20:08 -0800

    Implement manifest file and login and registeration pages

--- src/server/index.ts
+++ src/server/index.ts
@@ -8,15 +8,19 @@ import connectLiveReload from "connect-livereload";
 import livereload from "livereload";
 import {timeMiddleware} from "./middleware/time";
 
-dotenv.config();
 
-import authRoutes  from "./routes/auth";
-import rootRoutes  from "./routes/root";
-import gameRoutes  from "./routes/games";
-import lobbyRoutes from "./routes/lobby";
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+import * as routes from "./routes/manifest";
+import configureLiveReload from "./config/livereload";
+
+app.use("/", routes.home);
+app.use("/lobby", routes.mainLobby);
+app.use("/auth", routes.auth);
+app.use("/games", routes.games);
 
 app.use(morgan("dev"));
 app.use(express.json());
@@ -25,22 +29,11 @@ app.use(express.urlencoded({ extended: false }));
 const staticPath = path.join(process.cwd(), "src", "public");
 app.use(express.static(staticPath));
 
-if (process.env.NODE_ENV === "development") {
-    const reloadServer = livereload.createServer();
-
-    reloadServer.watch(staticPath);
-    app.use(connectLiveReload());
-}
+configureLiveReload(app, staticPath);
 
 app.use(cookieParser());
 app.set("views", path.join(process.cwd(), "src", "server", "views"));
 app.set("view engine", "ejs");
-
-// app.use(express.static(path.join(process.cwd(), "src", "public")));
-app.use("/", rootRoutes);
-app.use("/auth", authRoutes);
-app.use("/games", gameRoutes);
-app.use("/lobby", lobbyRoutes)
 
 app.use((_request, _response, next) => {
     next(httpErrors(404, "Page Not Found"));
---
+++ src/server/routes/manifest.ts
@@ -0,0 +1,7 @@
+export { default as auth } from "./auth";
+export { default as games } from "./games";
+export { default as mainLobby } from "./lobby";
+export { default as login } from "./login";
+export { default as register } from "./register";
+export { default as home } from "./root";
+export { default as configureLiveReload } from "../config/livereload";
