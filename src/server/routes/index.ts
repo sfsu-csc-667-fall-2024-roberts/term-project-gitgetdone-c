c87ed431ed845df5323d3d1e14d99a3634aba274
Milo5150 <dsimpson@sfsu.edu>
2024-12-18 15:04:45 -0800

    Fixed the game-state updating issue

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -5,3 +5,4 @@ export { default as mainLobby } from "./lobby";
 export { default as login } from "./login";
 export { default as register } from "./register";
 export { default as home } from "./root";
+export { default as configureLiveReload } from "../config/livereload";
